01939070dde56d17bc7f997bf2a6af7e3a767877
Josef Klafka <jlklafka@gmail.com>
2020-02-13 10:43:18 -0500

    python analysis

---
+++ bert_sdae.py
@@ -0,0 +1,242 @@
+import torch, random, unicodedata, re, logging, argparse
+import torch.nn as nn
+import torch.nn.functional as F
+from pytorch_pretrained_bert import BertTokenizer, BertModel
+from torch import optim
+
+MAX_LENGTH = 100
+HIDDEN_SIZE = 256
+NUM_ITERS = 75000
+device = torch.device("cuda:0")
+# device = torch.device("cpu")
+
+teacher_forcing_ratio = 0.5
+SOS_token = 0
+EOS_token = 1
+
+logging.basicConfig(filename = "noisy-results.csv", format="%(message)s", \
+                    level=logging.INFO)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("language_file", help="Name of the (noised) .txt to use")
+args = parser.parse_args()
+
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            if word not in self.word2index:
+                self.word2index[word] = self.n_words
+                self.word2count[word] = 1
+                self.index2word[self.n_words] = word
+                self.n_words += 1
+            else:
+                self.word2count[word] += 1
+
+# Lowercase, trim, and remove non-letter characters
+def normalizeString(s):
+    s = s.lower().strip()
+    s = ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+def readLang(language_file, reverse=False):
+    # Read the file and split into lines
+    lines = open('Stimuli/' + language_file + ".txt", encoding='utf-8').\
+        read().strip().split('\n')
+    # Split every line into pairs and normalize
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        lang_object = Lang(language_file)
+    else:
+        lang_object = Lang(language_file)
+
+    return lang_object, pairs
+
+
+def prepareData(lang, reverse=False):
+    lang_class, pairs = readLang(lang, reverse)
+    # new_pairs = []
+    # for pair in pairs:
+    #     print(pair)
+    #     if len(pair[0].split(' ')) < MAX_LENGTH and len(pair[1].split(' ')) < MAX_LENGTH:
+    #          new_pairs.append(pair)
+    pairs = [pair for pair in pairs if pair != [""] and len(pair[0].split(' ')) < MAX_LENGTH and \
+        len(pair[1].split(' ')) < MAX_LENGTH]
+    for pair in pairs:
+        lang_class.addSentence(pair[0])
+    return lang_class, pairs
+
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = F.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+
+        output = F.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = [lang.word2index[word] for word in sentence.split(' ')]
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+def train(input_text, target_tensor, model, tokenizer, decoder, \
+            decoder_optimizer, criterion, max_length=MAX_LENGTH):
+
+    decoder_optimizer.zero_grad()
+    target_length = target_tensor.size(0)
+
+    loss = 0
+
+    tokenized_text = tokenizer.tokenize(input_text)
+    #convert to vocabulary indices tensor
+    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+    tokens_tensor = torch.tensor([indexed_tokens])
+
+    tokens_tensor = tokens_tensor.to('cuda')
+
+    with torch.no_grad():
+        encoded_layers, _ = model(tokens_tensor)
+
+    encoder_outputs = encoded_layers[11]
+
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+
+    decoder_hidden = decoder.initHidden()
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        # Teacher forcing: Feed the target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  # Teacher forcing
+
+    else:
+        # Without teacher forcing: use its own predictions as the next input
+        for di in range(target_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach()  # detach from history as input
+
+            loss += criterion(decoder_output, target_tensor[di])
+            if decoder_input.item() == EOS_token:
+                break
+
+    loss.backward()
+
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
+
+
+def trainIters(lang, encoder, tokenizer, decoder, n_iters, learning_rate=0.01):
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    for i in range(n_iters):
+        pair_i = random.choice(pairs)
+        training_pairs = [(pair_i[0], tensorFromSentence(lang, pair_i[1]))]
+    criterion = nn.NLLLoss()
+
+    for iter in range(1, n_iters + 1):
+        training_pair = training_pairs[iter - 1]
+        input_text = training_pair[0]
+        target_tensor = training_pair[1]
+
+        loss = train(input_text, target_tensor, encoder, tokenizer,
+                     decoder, decoder_optimizer, criterion)
+
+
+def evaluate(lang, encoder, decoder, sentence, max_length=MAX_LENGTH):
+    with torch.no_grad():
+        input_tensor = tensorFromSentence(lang, sentence)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, \
+                                        device=device)
+
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                                     encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
+
+        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
+        decoder_hidden = encoder_hidden
+        decoded_words = []
+
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = \
+                decoder(decoder_input, decoder_hidden, encoder_outputs)
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                break
+            else:
+                decoded_words.append(lang.index2word[topi.item()])
+
+            decoder_input = topi.squeeze().detach()
+
+        return decoded_words
+
+
+# Load pre-trained model tokenizer (vocabulary)
+bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+# Load pre-trained model (weights)
+bert_model = BertModel.from_pretrained('bert-base-uncased')
+bert_model.to('cuda')
+bert_model.eval()
+
+lang, pairs = prepareData(args.language_file, True)
+attn_decoder = AttnDecoderRNN(HIDDEN_SIZE, lang.n_words, dropout_p=0.1).to(device)
+trainIters(lang, bert_model, bert_tokenizer, attn_decoder, NUM_ITERS)
+testing_pairs = [random.choice(pairs) for _ in range(1000)]
+for pair in testing_pairs:
+    guess = evaluate(lang, encoder, attn_decoder, pair[0])
+    guess = ' '.join(guess)
+    logging.info(pair[0] + ',' + guess + ',' + pair[1] + ',' + \
+                    str(int(guess == pair[1])))
