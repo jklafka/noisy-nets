2c66966e0a6fc7efcd44687e89f7155bdddc751b
Josef Klafka <jlklafka@gmail.com>
2020-03-04 13:53:05 -0500

    bert preprocess and stimuli

--- bert_preprocess.py
+++ bert_preprocess.py
@@ -12,6 +12,18 @@ parser.add_argument("vocab_file", help="Where to print the vocab file")
 args = parser.parse_args()
 
 
+def get_BERT_vocab(tokenizer, pairs):
+    '''
+    Get one-hot indices for each BERT-token in language_file.
+    '''
+    vocab = set()
+    for pair in pairs:
+        for line in pair:
+            tokens = tokenizer.tokenize(line)
+            vocab = vocab | set(tokens)
+    vocab = ["SOS", "EOS"] + list(vocab) # insert SOS and EOS tokens
+    return vocab
+
 # set up training and testing data
 pairs = open("Stimuli/" + args.language_file + ".txt", 'r').readlines()
 pairs = [line.strip('\n').split('\t') for line in pairs]
@@ -28,10 +40,10 @@ pairs_train, pairs_test = train_test_split(pairs, test_size = 0.1, \
                                             stratify = lengths)
 # write training and testing pairs to separate files
 with open("Stimuli/" + args.train_file + ".txt", 'w') as filename:
-        filename.writelines("%s\t%s" % pair[0], pair[1] for pair in pairs_train)
+        filename.writelines("%s\t%s" % (pair[0], pair[1]) for pair in pairs_train)
 
 with open("Stimuli/" + args.test_file + ".txt", 'w') as filename:
-        filename.writelines("%s\t%s" % pair[0], pair[1] for pair in pairs_test)
+        filename.writelines("%s\t%s" % (pair[0], pair[1]) for pair in pairs_test)
 
 
 # get vocabulary and print to external file
@@ -39,17 +51,4 @@ bert_tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 vocab = get_BERT_vocab(bert_tokenizer, pairs)
 
 with open("Stimuli/" + args.vocab_file + ".txt", 'w') as filename:
-        filename.writelines("%s\t%d" % word, index for word, index in enumerate(vocab))
-
-
-def get_BERT_vocab(tokenizer, pairs):
-    '''
-    Get one-hot indices for each BERT-token in language_file.
-    '''
-    vocab = set()
-    for pair in pairs:
-        for line in pair:
-            tokens = tokenizer.tokenize(line)
-            vocab = vocab | set(tokens)
-    vocab = ["SOS", "EOS"] + list(vocab) # insert SOS and EOS tokens
-    return vocab
+        filename.writelines("%s\t%d" % (word, index) for index, word in enumerate(vocab))
