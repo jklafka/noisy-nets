4c3f74150f2ff50e38f5da4b402063fb979c069f
Josef Klafka <jlklafka@gmail.com>
2020-04-03 13:57:29 -0400

    remove copies of slurm scripts

--- clear-nets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=clear-nets
-#SBATCH --error=clear-nets.err
-#SBATCH --output=clear-nets.out
-#SBATCH --mail-user=jklafka@andrew.cmu.edu
-#SBATCH --mail-type=END,FAIL
-
-python3 bert_preprocess.py $1 train test vocab
-python3 bert_sdae.py train test vocab
--- noisy-nets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=noisy-nets
-#SBATCH --error=noisy-nets.err
-#SBATCH --output=noisy-nets.out
-#SBATCH --mail-user=jklafka@andrew.cmu.edu
-#SBATCH --mail-type=END,FAIL
-
-python3 bert_sdae.py noisy-train noisy-test vocab
