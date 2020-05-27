e78645865bba7a301e24fd0fb76cdf4a434d3c1d
Josef Klafka <jlklafka@gmail.com>
2020-05-27 14:14:59 -0400

    new train test vocab

--- preprocess.sh
+++ preprocess.sh
@@ -3,4 +3,4 @@
 python3 Noise/clean_script.py $1
 python3 Noise/noise.py clean_$1
 python3 Noise/train_test_vocab.py noisy-clean_$1 ${1}_train ${1}_test ${1}_vocab
-rm Stimuli/clean_eng-eng.txt Stimuli/eng-eng_vocab.txt Stimuli/noisy-clean_eng-eng.csv
+rm Stimuli/clean_$1.txt Stimuli/noisy-clean_$1.csv
